55be1e6b0ef5af8d992aa258b31cf33d01a87d69
Div Vats <vats.div@gmail.com>
2014-08-12 15:15:16 -0400

    First implementation of using mallet to rank items, so far only works when user inputs on search item

--- RankingUsingMallet.py
+++ RankingUsingMallet.py
@@ -15,23 +15,132 @@ Created on Mon Aug 11 12:03:01 2014
 #       word_topic_counts.txt
 
 import numpy as np
+import pandas as pd
 import gzip
+from scipy.sparse import lil_matrix
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import normalize
+
+def GetID(text):
+    text = text.split()
+    ind = text[1]
+    ind = ind.split('.txt')[0]
+    ind = int(ind.split('guide_data/')[1]) # get index number
+    return ind
 
 def ReadDocumentTopics(num_docs, num_topics):   
     # read document in ./mallet_output/per_document_output.gz
-    
+    # and count the number of times a topic appears
+
     # read file
     f = gzip.open('./mallet_output/per_document_output.gz', 'rb')
-    
-    doc_tops = np.empty([num_docs,num_topics])
+    f.readline()
+    alpha = f.readline()
+    alpha = alpha.split(":")[1]
+    alpha = np.array([float(a) for a in alpha.split()])
+    f.readline()
+    doc_tops = np.zeros([num_docs,num_topics])
     
     for chunk in iter(lambda: f.readline(), ''):
         
-        # split document
+        ind = GetID(chunk) # id number
+        top_num = int(chunk.split()[-1]) # get topic number
+        if ind > num_docs-1:
+            break
+        doc_tops[ind, top_num] = doc_tops[ind, top_num] + 1.0
+        
+    return alpha, doc_tops
+        
+def ReadTopicProps(num_docs, num_topics):
+
+    f = open("mallet_output/topic_props.txt")
+    f.readline()
+    topic_prop = np.zeros((num_docs, num_topics))
+    
+    for chunk in iter(lambda: f.readline(), ''):
+        ind = GetID(chunk)
+        chunk = chunk.split()[2:]
+        for i in range(num_topics):
+            c1 = int(chunk[2*i])
+            c2 = float(chunk[2*i+1])
+            topic_prop[ind,c1] = c2
+    
+    return topic_prop
+    
+def ReadWordCounts(num_topics):
+    
+    f = open("mallet_output/word_topic_counts.txt")
+
+    num_words = 10000
+
+    # create a sparse matrix of size num_words x num_topics
+    
+    word_count = lil_matrix((num_words,num_topics))
+    word_list = []
+    
+    for chunk in iter(lambda: f.readline(), ''):
         chunk = chunk.split()
-        ind = chunk[1]
-        ind = ind.split('.txt')[0]
-        ind = int(ind.split('/')[1]) # get indx number
-        top_num = int(chunk[-1]) # get topic number
-        
-        
+        ind = int(chunk[0])
+        if ind == num_words-1:
+            break
+        word_list.append(chunk[1])
+        chunk = chunk[2:]
+        for ck in chunk:
+            # ck has the form xx:yy
+            ck = ck.split(':')
+            word_count[ind,int(ck[0])] = int(ck[1])
+                
+    return word_count, np.array(word_list)
+    
+def ScoreDocument(X, word_count,total_words_eachtopic,topic_props,num_words,i):
+    
+    # score = (# words in doc) * \sum[ (# times word in T_i) / (# words) * P(T_i) ]    
+    tmp = np.array(word_count[i,:].todense()).flatten()
+    tmp = tmp / total_words_eachtopic 
+    tmp = np.dot(topic_props, tmp)
+    xx = np.array(X[:,i].todense()).flatten()
+
+    mix = 0.5
+    
+    tmp = num_words * ((1-mix) * tmp + mix * xx)
+    return tmp
+    
+main_data = pd.read_csv("./data/FilteredTravelData.csv")
+guide_data = np.array(main_data["all_data"])
+
+title = np.array(main_data["title"])
+num_docs = len(main_data)
+
+num_topics = 100
+
+# doc_tops: num_docs x num_topics matrix
+# topic_props: num_docs x num_topics matrix
+# num_words: num_docs x 1 vector
+alpha, doc_tops = ReadDocumentTopics(num_docs, num_topics)
+num_words = np.sum(doc_tops,axis=1)
+topic_props = ReadTopicProps(num_docs, num_topics)
+
+# word_distr: num_words x num_topics matrix
+word_count, word_list = ReadWordCounts(num_topics)
+total_words_eachtopic = np.array(word_count.sum(axis=0))[0]
+
+vectorizer = TfidfVectorizer(vocabulary=word_list)
+X = vectorizer.fit_transform(guide_data)
+X = normalize(X,axis=1,norm='l1')
+
+# is keyword is index_keyword then score is
+search_word = ['bourbon']
+ListOfKeywords = [np.where(word_list == ww)[0] for ww in search_word]
+score = np.zeros([num_docs,1])
+score = 0
+for index_keyword in ListOfKeywords:
+    score = score + ScoreDocument(X, word_count,total_words_eachtopic,topic_props,num_words,index_keyword)
+
+ranked_list = np.argsort(-np.array(score))
+title_r = title[ranked_list]
+
+#word_num = np.array([len(text.split()) for text in main_data["all_data"]])
+#topic1 = doc_tops[:,3]
+#ranked_list = np.argsort(-np.array(topic1))
+#title_r = title[ranked_list]
