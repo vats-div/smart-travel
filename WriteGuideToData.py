4912e465ea55039a534116daa4667d4cded12d36
Div Vats <vats.div@gmail.com>
2014-08-14 21:23:55 -0400

    code to write the guide to sql

---
+++ WriteGuideToData.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 13 15:01:26 2014
+
+@author: dvats
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  4 15:10:42 2014
+
+@author: dvats
+"""
+
+"""
+1) Write function to compute entropy from word frequencies
+2) First attempt was to compute entropy of words individually
+    and then look at the distributio of the words -> didn't work that well
+3) To do -> now look at a more global approach that uses topic modeling
+"""
+
+import numpy as np
+import pandas as pd
+import MySQLdb as db
+
+# read the ./data/TravelData.csv
+
+main_data = pd.read_csv("./data/FilteredTravelData.csv").fillna(value='')
+num_words = np.array([len(gg.split()) for gg in main_data["all_data"]])
+title = np.array(main_data["title"])
+region = np.array(main_data["Region"])
+top_words = np.array(main_data["top_words"])
+url = np.array(main_data["url"])
+num_rows = len(main_data)
+
+#Open connection to mysql database
+con = db.connect('localhost','root','','guide_data')
+
+# cursors
+cr = con.cursor()
+cr.execute("drop table if exists MainData;")
+
+# create table columns
+tmp = '''
+create table MainData (
+        id int(7),
+        title text,
+        top_words blob,
+        region text,
+        url text,
+        num_words int(7),
+        primary key (id)
+);
+'''
+cr.execute(tmp)
+
+for i in range(num_rows): 
+    print i
+    tmp_c = "INSERT INTO MainData (id, title, top_words, region, num_words, url) VALUES (%s, %s, %s, %s, %s, %s)" 
+    if type(region[i]) == type(1.0):
+        region[i] = ' '
+    tmp_v = [i+1, title[i], top_words[i], region[i].replace('_',' '), num_words[i], url[i]]
+    cr.execute(tmp_c,tuple(tmp_v))
+
+con.commit()
+cr.close()
+con.close()
