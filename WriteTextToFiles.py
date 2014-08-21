608eefec89b2b420a2819aed63bf67342bb9eff7
Div Vats <vats.div@gmail.com>
2014-08-21 09:40:42 -0400

    Code to write title, countries, etc to file so that error checking can be done

---
+++ WriteTextToFiles.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 19 11:50:45 2014
+
+@author: dvats
+"""
+
+# get a list of all words, regions, and cities and write to file
+
+import pandas as pd
+import numpy as np
+
+ff = open('./mallet_output/word_topic_counts.txt', 'r')
+
+words = ff.readlines()
+words = [w.split()[1] for w in words]
+words = ' '.join(words)
+# write words to a text file
+
+ff = open('./test_web/word.txt','w')
+ff.write(words)
+ff.close()
+
+main_data = pd.read_csv("./data/FilteredTravelData.csv", usecols=["Region","title"]).fillna(value='')
+title = np.array(main_data["title"])
+title = [tt.lower() for tt in title]
+
+ff = open('./test_web/title.txt','w')
+ff.write(' '.join(title))
+ff.close()
+
+region = np.array(main_data["Region"])
+region = [rr.lower() for rr in region]
+
+ff = open('./test_web/country.txt','w')
+ff.write(' '.join(region))
+ff.close()
